000f1d50be10a7a1c2015e47c218288a52a64727
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2020-07-29 14:55:39 +0300

    Add user permissions

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -37,6 +37,19 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route       POST /api/v1/bootcamps
 // access       Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    // Add user to req.body
+    req.body.user = req.user.id;
+
+    // Check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({
+        user: req.user.id
+    });
+
+    if (publishedBootcamp && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`This user with ID ${req.user.id} has already published a bootcamp`, 400));
+    }
+
+
     const bootcamp = await Bootcamp.create(req.body);
 
     res.status(201).json({
@@ -63,8 +76,8 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
     res.status(200).json({
         success: true,
         data: bootcamp
-    })
-})
+    });
+});
 
 
 
@@ -124,7 +137,7 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
         success: true,
         count: bootcamps.length,
         data: bootcamps
-    })
+    });
 
 });
 
