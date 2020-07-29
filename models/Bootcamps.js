100cb88e1acb48f48860c080598faa656456b021
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2020-07-29 14:38:49 +0300

    Add User Relationship

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -97,6 +97,11 @@ const BootcampSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
 }, {
     toJSON: {
