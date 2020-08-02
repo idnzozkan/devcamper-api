1462ccb3903f86f5270831fa8a1ddfc3f88559b3
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2020-08-02 21:53:45 +0300

    Add reviews data

--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({
 const Bootcamp = require('./models/Bootcamps');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 const {
     DESTRUCTION
@@ -29,6 +30,7 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8'));
 
 // Import into DB
 const importData = async () => {
@@ -36,6 +38,7 @@ const importData = async () => {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
         await User.create(users);
+        await Review.create(reviews);
         console.log('Data Imported...'.green.inverse);
         process.exit();
     } catch (err) {
@@ -50,6 +53,7 @@ const deleteData = async () => {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
         await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
     } catch (err) {
