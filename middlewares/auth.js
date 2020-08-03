dc8ed2b671d12280f6d77fd453bddc826c4eb3d1
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2020-08-03 15:09:15 +0300

    Add Logout user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,16 +9,18 @@ exports.protect = asyncHandler(async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        // Set token from Bearer token in header
         token = req.headers.authorization.split(' ')[1];
     }
 
-    // else if (req.cookies.token) {
-    //     token = req.cookies.token;
-    // }
+    // Set token from cookie
+    else if (req.cookies.token) {
+        token = req.cookies.token;
+    }
 
     // Make sure token exist
     if (!token) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 
     try {
@@ -26,7 +28,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
         req.user = await User.findById(decoded.id);
         next();
     } catch (err) {
-        return next(new ErrorResponse('Not authorize to access this route', 401));
+        return next(new ErrorResponse('Not authorized to access this route', 401));
     }
 });
 
